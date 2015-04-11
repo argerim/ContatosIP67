49b36c7800d2c183778eb6929944ac5a86b1b545
ios5065 <ios5065@Mac-mini-de-Caelum-11.local>
2015-04-11 14:13:48 -0300

    Initial Commit

---
+++ ContatosIP67/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContatosIP67
+//
+//  Created by ios5065 on 11/04/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
